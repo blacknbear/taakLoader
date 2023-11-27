1fe75d7b1c081d124a8ae88c66a70c7500928264
Blacknbear <64272406+blacknbear@users.noreply.github.com>
2023-11-27 22:13:45 +0330

    taakLoader first commit!

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import Lottie, {LottieRefCurrentProps} from "lottie-react";
+import animationData from './Assets/taakLoading.json';
+import { useRef } from "react";
 
 function App() {
+  const taakLoader = useRef();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{width: 150}}>
+      <Lottie autoPlay="true" lottieRef={taakLoader} animationData={animationData} />
     </div>
   );
 }
